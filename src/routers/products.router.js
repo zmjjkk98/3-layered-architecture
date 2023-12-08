c49a9a64bc177fb008a2141f4226bf3f2dfcec76
zmjjkk98 <ysyt3199@gmail.com>
2023-12-09 00:58:14 +0900

    feat: [router] getById 라우터 컨트롤러 추가

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -6,7 +6,7 @@ const productsController = new ProductsController();
 
 productsRouter.post("/", productsController.createProduct);
 productsRouter.get("/", productsController.getProduct);
-productsRouter.get("/:productId", productsController);
+productsRouter.get("/:productId", productsController.getProductById);
 productsRouter.put("/:productId", productsController.putProduct);
 productsRouter.delete("/:productId", productsController.deleteProduct);
 
