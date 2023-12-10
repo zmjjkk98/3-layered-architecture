f644b33610cca71791e2f507132114df1bfaebf3
zmjjkk98 <ysyt3199@gmail.com>
2023-12-10 14:16:03 +0900

    feat: [main] user 유효성 검증 로직 추가

--- src/service/users.service.js
+++ src/service/users.service.js
@@ -7,12 +7,52 @@ export class UsersService {
 
   signUp = async (email, password, passwordConfirm, name) => {
     try {
+      if (!email) {
+        return res.status(400).json({ message: '이메일 입력이 필요합니다.' });
+      }
+
+      if (!password) {
+        return res.status(400).json({ message: '비밀번호 입력이 필요합니다.' });
+      }
+
       if (password !== passwordConfirm) {
-        throw new Error('');
+        return res.status(400).json({
+          message: '비밀번호 확인 입력이 필요합니다.',
+        });
+      }
+
+      if (!name) {
+        return res.status(400).json({ message: '이름 입력이 필요합니다.' });
+      }
+
+      if (password !== passwordConfirm) {
+        return res
+          .status(400)
+          .json({ message: '입력 한 비밀번호가 서로 일치하지 않습니다.' });
+      }
+
+      if (password.length < 6) {
+        return res.status(400).json({
+          message: '비밀번호는 최소 6자리 이상입니다.',
+        });
+      }
+
+      let emailValidationRegex = new RegExp('[a-z0-9._]+@[a-z]+.[a-z]{2,3}');
+      const isValidEmail = emailValidationRegex.test(email);
+      if (!isValidEmail) {
+        return res.status(400).json({
+          message: '올바른 이메일 형식이 아닙니다.',
+        });
+      }
+
+      const existedUser = await this.usersRepository.findUserByEmail(email);
+      if (existedUser) {
+        return res.status(400).json({
+          message: '이미 가입 된 이메일입니다.',
+        });
       }
 
       const saltround = process.env.PASSWORD_HASH_SALT_ROUNDS;
-
       const bcryptPassword = bcrypt.hashSync(password, +saltround);
 
       const user = await this.usersRepository.signUp(
@@ -30,24 +70,36 @@ export class UsersService {
       };
     } catch (error) {
       console.log(error);
+      next(error);
     }
   };
 
   signIn = async (email, password) => {
-    const user = await this.usersRepository.findUserByEmail(email);
+    try {
+      if (!email) {
+        return res.status(400).json({ message: '이메일 입력이 필요합니다.' });
+      }
 
-    const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+      if (!password) {
+        return res.status(400).json({ message: '비밀번호 입력이 필요합니다.' });
+      }
 
-    const accessToken = jwt.sign(
-      { userId: user.userId },
-      process.env.JWT_ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: '10h',
-      },
-    );
+      const user = await this.usersRepository.findUserByEmail(email);
 
-    return {
-      accessToken,
-    };
+      const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+      const accessToken = jwt.sign(
+        { userId: user.userId },
+        process.env.JWT_ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: '10h',
+        },
+      );
+
+      return { accessToken };
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   };
 }
