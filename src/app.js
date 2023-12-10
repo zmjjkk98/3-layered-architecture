050587cdd0df42bef2f47fc58176eae8fb54879d
zmjjkk98 <ysyt3199@gmail.com>
2023-12-10 14:57:23 +0900

    feat: [main] 에러처리 미들웨어 추가

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { apiRouter } from './routers/index.js';
 import 'dotenv/config';
+import errorHandlerMiddleware from './middleware/error-handler.middleware.js';
 
 const app = express();
 const PORT = process.env.SERVER_PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api', apiRouter);
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요 !');
--- src/repository/products.repositroy.js
+++ src/repository/products.repositroy.js
@@ -1,4 +1,4 @@
-import { prisma } from "../utils/prisma/index.js";
+import { prisma } from '../utils/prisma/index.js';
 
 export class ProductsRepository {
   createProduct = async (title, description, userId) => {
